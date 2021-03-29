09185359bec130677b9a9ca59042978ed7ab2af8
sweetylenka <sweetylenka2013@gmail.com>
2021-03-29 12:05:41 +0530

    VALIDATING A PINCODE

---
+++ RegexPincode.java
@@ -0,0 +1,15 @@
+package pincode;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+public class RegexPincode{
+	
+public static void main(String args[]) {
+	System.out.println("WELCOME TO JAVA REGEX - VALIDATING A PINCODE");
+	
+	
+
+ }
+}
